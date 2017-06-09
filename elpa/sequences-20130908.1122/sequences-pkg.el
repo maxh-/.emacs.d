b23d7be3a40f010595f8fbdab6743c3a0c6a2508
Max Hansson <maxh88@gmail.com>
2017-06-09 10:36:49 +0200

    some javascript packages

---
+++ elpa/sequences-20130908.1122/sequences-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sequences" "20130908.1122" "Ports of some Clojure sequence functions." '((emacs "24")) :commit "2bcc54a6738202fffb23e9579b9695337ba6ef26" :keywords '("convenience"))
