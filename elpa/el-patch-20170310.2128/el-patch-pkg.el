8b7bbce82a5c2cbcc5edad5d4194369843f2f1c9
Max Hansson <maxh88@gmail.com>
2017-06-10 12:06:45 +0200

    stuff

--- elpa/el-patch-20170310.2128/el-patch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "el-patch" "20170310.2128" "Future-proof your Emacs Lisp customizations!" '((emacs "25")) :url "https://github.com/raxod502/el-patch" :keywords '("extensions"))
