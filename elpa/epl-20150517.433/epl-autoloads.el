2f24d08b0a709aa9c523c0c6543ad838c173fb71
Max Hansson <maxh88@gmail.com>
2017-06-18 21:50:45 +0200

    delete elpa dir

--- elpa/epl-20150517.433/epl-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("epl.el") (22854 45803 690882 826000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epl-autoloads.el ends here
--- elpa/lisp-extra-font-lock-20160930.1227/lisp-extra-font-lock-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lisp-extra-font-lock" "20160930.1227" "Highlight bound variables and quoted exprs." 'nil :commit "092f5a6e75ddfc8051b252f10e182723a17980e4" :url "https://github.com/Lindydancer/lisp-extra-font-lock" :keywords '("languages" "faces"))
